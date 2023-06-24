7cba0f9a534381f335b753b9314e2acd1d5d0559
westerntc <veretax@gmail.com>
2023-06-24 08:01:25 -0400

    Note WPF projects had to be moved to .Net 7.0 to be compatilble We added a DataManagementLibrary to handle the ToJson method for a person or List<PersonModel>

---
+++ DataManagementLibrary/Extensions/PersonExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using PersonAddressLibrary.Framework.Models;
+using Formatting = System.Xml.Formatting;
+
+namespace DataManagementLibrary.Extensions;
+
+public static class PersonExtensions
+{
+    public static void SaveAsJson(this PersonModel person, string filePath)
+    {
+        string json = JsonConvert.SerializeObject(person, (Newtonsoft.Json.Formatting)Formatting.Indented);
+        File.WriteAllText(filePath, json);
+    }
+
+    public static void SaveAsJson(this List<PersonModel> list, string filePath)
+    {
+        string json = JsonConvert.SerializeObject(list, (Newtonsoft.Json.Formatting)Formatting.Indented);
+        File.WriteAllText(filePath, json);
+    }
+}
