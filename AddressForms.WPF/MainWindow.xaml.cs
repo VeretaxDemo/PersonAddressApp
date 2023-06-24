3646fd2ebf768ffd83f972516b92b44b9a3f5997
westerntc <veretax@gmail.com>
2023-06-24 08:06:28 -0400

    Alert suggested I needed to add WPF Annotations for the AddressForms WPF (I wonder if this was not actually needd though) Added Export Button that lights up on personEntry when person's information (except for address) is not empty Export will prompt for a file path to save a json file for this person, and will autosuggest a name in the lastname.firstname.json format Lastly, i wanted an ExportAll button that lit up when you had at least 1 Person in the listbox, and would output the entire list.

--- AddressForms.WPF/MainWindow.xaml.cs
+++ AddressForms.WPF/MainWindow.xaml.cs
@@ -15,23 +15,76 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using PersonAddressLibrary.Framework;
 using PersonAddressLibrary.Framework.Models;
+using DataManagementLibrary.Extensions;
 
 namespace AddressForms.WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, ISavePersonModel
+    public partial class MainWindow : Window, ISavePersonModel, INotifyPropertyChanged
     {
-        public BindingList<IPersonModel> people = new BindingList<IPersonModel>();
+        private BindingList<IPersonModel> _people = new BindingList<IPersonModel>();
+
+        public BindingList<IPersonModel> People
+        {
+            get { return _people; }
+            set
+            {
+                _people = value;
+                OnPropertyChanged(nameof(People));
+                UpdateExportAllButtonState();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _isExportAllEnabled = false;
+        public bool IsExportAllEnabled
+        {
+            get { return _isExportAllEnabled; }
+            set
+            {
+                _isExportAllEnabled = value;
+                OnPropertyChanged(nameof(IsExportAllEnabled));
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
 
-            this.peopleListBox.ItemsSource = people;
+            People.ListChanged += People_ListChanged; // Subscribe to the ListChanged event
+
+            this.peopleListBox.ItemsSource = People;
+
+            DataContext = this;
         }
 
-        private void addPersonButton_Click(object sender, RoutedEventArgs e)
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void UpdateExportAllButtonState()
+        {
+            if (People != null)
+            {
+                IsExportAllEnabled = People.Count() >= 1 ? true : false;
+                exportAllButton.IsEnabled = true;
+            }
+            else
+            {
+                IsExportAllEnabled = false;
+                exportAllButton.IsEnabled = false;
+            }
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            People.ListChanged += People_ListChanged;
+        }
+
+        private void AddPersonButton_Click(object sender, RoutedEventArgs e)
         {
             Person_Entry newPersonEntry = new Person_Entry(this);
 
@@ -39,13 +92,43 @@ namespace AddressForms.WPF
             newPersonEntry.Show();
 
 
-            this.peopleListBox.ItemsSource = this.people;
+            this.peopleListBox.ItemsSource = this.People;
         }
 
         public void SavePerson(IPersonModel person)
         {
-            people.Add(person);
+            People.Add(person);
 
+            // Call the UpdateExportAllButtonState method after adding the first person
+            if (People.Count >= 1)
+            {
+                UpdateExportAllButtonState();
+            }
+
+        }
+
+        private void ExportAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (People.Count == 0)
+            {
+                MessageBox.Show("There are no persons to export.", "Export All", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+            saveFileDialog.Filter = "JSON Files (*.json)|*.json";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                List<PersonModel> allPersons = People.Cast<PersonModel>().ToList();
+                allPersons.SaveAsJson(filePath);
+            }
+        }
+
+        private void People_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            UpdateExportAllButtonState();
         }
     }
 }
--- AddressForms.WPF/PersonEntry.xaml.cs
+++ AddressForms.WPF/PersonEntry.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,20 +13,75 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using AddressForms.WPF.Annotations;
 using PersonAddressLibrary.Framework;
 using PersonAddressLibrary.Framework.Models;
 using PersonAddressLibrary.Framework.Models.Addresses;
+using DataManagementLibrary.Extensions;
 
 namespace AddressForms.WPF
 {
     /// <summary>
     /// Interaction logic for Person_Entry.xaml
     /// </summary>
-    public partial class Person_Entry : Window, ISaveAddress
+    public partial class Person_Entry : Window, ISaveAddress, INotifyPropertyChanged
     {
 
         private ISavePersonModel _parent;
         BindingList<IPersonAddressModel> Addresses = new BindingList<IPersonAddressModel>();
+
+
+
+        private bool _isExportEnabled;
+        public bool IsExportEnabled
+        {
+            get { return _isExportEnabled; }
+            set
+            {
+                _isExportEnabled = value;
+                OnPropertyChanged(nameof(IsExportEnabled));
+            }
+        }
+
+        private void UpdateExportButtonState()
+        {
+            IsExportEnabled = !string.IsNullOrEmpty(FirstName) &&
+                              !string.IsNullOrEmpty(LastName) &&
+                              !string.IsNullOrEmpty(Email);
+        }
+
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                UpdateExportButtonState();
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                UpdateExportButtonState();
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                UpdateExportButtonState();
+            }
+        }
 
         public Person_Entry(ISavePersonModel parent)
         {
@@ -33,15 +89,23 @@ namespace AddressForms.WPF
             _parent = parent;
 
             addressListBox.ItemsSource = Addresses;
+
+            DataContext = this;
         }
 
-        private void addAddressButton_Click(object sender, RoutedEventArgs e)
+        //private void OnPropertyChanged(string propertyName)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+
+
+        private void AddAddressButton_Click(object sender, RoutedEventArgs e)
         {
             PersonAddressEntry addressEntry = new PersonAddressEntry(this);
             addressEntry.Show();
         }
 
-        private void backButton_Click(object sender, RoutedEventArgs e)
+        private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
@@ -53,9 +117,18 @@ namespace AddressForms.WPF
 
         }
 
-        private void saveButton_Click(object sender, RoutedEventArgs e)
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
 
+            var person = PreparePersonModel();
+
+            _parent.SavePerson(person);
+
+            this.Close();
+        }
+
+        private PersonModel PreparePersonModel()
+        {
             string firstName = firstNameTextBox.Text;
             string lastName = lastNameTextBox.Text;
             string email = emailTextBox.Text;
@@ -67,10 +140,35 @@ namespace AddressForms.WPF
                 Email = email,
                 Addresses = this.Addresses.ToList(),
             };
+            return person;
+        }
 
-            _parent.SavePerson(person);
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            PersonModel person = PreparePersonModel();
 
-            this.Close();
+            Microsoft.Win32.SaveFileDialog saveFileDialog = new Microsoft.Win32.SaveFileDialog();
+            saveFileDialog.Filter = "JSON Files (*.json)|*.json";
+
+
+            // Generate the suggested filename using the format "lastname.firstname.json"
+            string suggestedFileName = $"{person.LastName}.{person.FirstName}.json";
+            saveFileDialog.FileName = suggestedFileName;
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filePath = saveFileDialog.FileName;
+                person.SaveAsJson(filePath);
+            }
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
